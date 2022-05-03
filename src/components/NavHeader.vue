3cf564337d5aa9cd9363624da4db2cbce9a243ab
AC <1316534834@qq.com>
2022-05-03 14:55:29 +0800

    1

---
+++ src/components/NavHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'nav-header',
+  data () {
+    return {}
+  },
+  methods: {},
+}
+</script>
+
+<style scoped>
+</style>
