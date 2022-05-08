54fcded6d6d24acd2e0ed20a1d39a666f2570a24
AC <1316534834@qq.com>
2022-05-08 12:10:47 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ axios.defaults.baseURL = '/api'
 axios.defaults.timeout = 8000
 //接口错误拦截
 axios.interceptors.response.use(function (response) {
-  let res = responde.data
+  let res = response.data
   if (res.status === 0) {
     return res.data
   } else if (res.status === 10) {
